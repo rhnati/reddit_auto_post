c45deb5d9035ed29feeb006036650d0a68af6324
Roman Hnatiuk <roman.hnatiuk02@gmail.com>
2023-12-07 00:50:59 +0200

    fix

--- reddit_auto_post.js
+++ reddit_auto_post.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const clientId = "LLLaFpRNu8BqnFUVDPsvmw";
 const clientSecret = "PYx5Pchx8cpOci-7UwKBerz47yNE4A";
-const username = "SportScoreio";
+const username = "SportScore-Poster";
 const password = "Aa1234567!@a";
-const subreddit = "SportScoreIO";
+const subreddit = "SportScoreioCommunity";
 
 const postedMatches = new Set();
 
