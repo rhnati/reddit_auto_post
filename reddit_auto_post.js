45d5ed169bdcb5bb7355f3176c5c0740d6c99e1e
Roman Hnatiuk <roman.hnatiuk02@gmail.com>
2024-01-16 13:51:44 +0200

    fix

--- reddit_auto_post.js
+++ reddit_auto_post.js
@@ -8,37 +8,60 @@ const subreddit = "SportScore_io";
 
 const postedMatches = new Set();
 let matchIndex = 0;
+let autopostData;
 
-function fetchData() {
-  fetch(
-    "https://sportscore.io/api/v1/football/matches/?match_status=live&sort_by_time=false&page=0",
-    {
-      method: "GET",
+//Get autopost is on or off
+async function fetchAutopost() {
+  try {
+    const response = await fetch('https://sportscore.io/api/v1/autopost/settings/reddit/', {
+      method: 'GET',
       headers: {
-        accept: "application/json",
-        "X-API-Key": "uqzmebqojezbivd2dmpakmj93j7gjm",
+        "accept": "application/json",
+        'X-API-Key': 'uqzmebqojezbivd2dmpakmj93j7gjm',
       },
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      processData(data.match_groups);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
     });
+    const data = await response.json();
+    autopostData = data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-function processData(matchGroups) {
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://sportscore.io/api/v1/football/matches/?match_status=live&sort_by_time=false&page=0",
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          "X-API-Key": "uqzmebqojezbivd2dmpakmj93j7gjm",
+        },
+      }
+    );
+
+    const data = await response.json();
+    processData(data.match_groups);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function processData(matchGroups) {
   try {
     if (!Array.isArray(matchGroups)) {
       console.error("Invalid matchGroups:", matchGroups);
       return;
     }
 
-    matchGroups.forEach((matchGroup) => {
-      getMatch(matchGroup);
-    });
+    await fetchAutopost();
+    console.log(autopostData);
+    
+    if (autopostData[0].enabled) {
+      matchGroups.forEach((matchGroup) => {
+        getMatch(matchGroup);
+      });
+    }
   } catch (error) {
     console.error("Error processing data:", error);
   }
